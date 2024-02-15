16fc6901be0128eede9fac79424e88fa74586199
Documenter.jl <documenter@juliadocs.github.io>
2024-02-15 16:33:40 +0000

    build based on 3259e1d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = sEMTor","category":"page"},{"location":"#sEMTor","page":"Home","title":"sEMTor","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for sEMTor.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [sEMTor]","category":"page"}]
+}
